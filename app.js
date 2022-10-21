68458ca3e4e775af98a6d4dc7f33c89bdc83daac
VladimirPariy <vladimir.pariy@gmail.com>
2022-10-21 14:20:22 +0300

    added server logging

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import {usersRouter} from "./app/users/usersRouter.js";
 import {authRouter} from "./app/auth/authRouter.js";
+import {logger} from './app/logging.js';
 
 const PORT = process.env.PORT || 3000;
+const HOST = 'localhost'
+
 
 const app = express();
 
@@ -15,9 +18,11 @@ app.use('/api', authRouter);
 
 const startApp = async () => {
     try {
-        app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+        app.listen(PORT);
+        logger.info(`Server started and running on http://${HOST}:${PORT}`)
     } catch (e) {
         console.log(e);
+        logger.error(e.message)
     }
 }
 startApp()
