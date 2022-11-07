bf7088a78c91ea57385a12c80e26b870baafb9f3
VladimirPariy <vladimir.pariy@gmail.com>
2022-11-07 18:14:53 +0200

    added objection

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import {usersRouter} from "./app/users/usersRouter.js";
 import {authRouter} from "./app/auth/authRouter.js";
 import {logger} from './app/logging.js';
+import {setupDB} from "./DB/setupDB.js";
 
 
 const PORT = process.env.PORT || 3000;
@@ -9,6 +10,7 @@ const HOST = 'localhost'
 
 
 const app = express();
+
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -18,11 +20,12 @@ app.use('/api', authRouter);
 
 
 const startApp = async () => {
-	try {
-		app.listen(PORT);
-		logger.info(`Server started and running on http://${HOST}:${PORT}`)
-	} catch (e) {
-		logger.error(e.message)
-	}
+  try {
+    app.listen(PORT);
+    setupDB();
+    logger.info(`Server started and running on http://${HOST}:${PORT}`)
+  } catch (e) {
+    logger.error(e.message)
+  }
 }
 startApp()
